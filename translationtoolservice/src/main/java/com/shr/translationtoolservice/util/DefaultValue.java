ed379da4e48ed389b5a8b11aa32d45792248aa29
赵鸿鹏 <@sp5000.com>
2024-04-30 09:36:11 +0800

    delete

--- translationtoolservice/src/main/java/com/shr/translationtoolservice/util/DefaultValue.java
+++
@@ -1,8 +0,0 @@
-package com.shr.translationtoolservice.util;
-
-import java.lang.annotation.*;
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.FIELD)
-public @interface DefaultValue {
-    String value() default "";
-}
