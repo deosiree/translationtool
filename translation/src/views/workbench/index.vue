ff3d31011945aff380b386a145c499b9a7c1d0c8
Spindrfit <1692592987@qq.com>
2025-02-19 18:13:45 +0800

    index

--- translation/src/views/workbench/index.vue
+++ translation/src/views/workbench/index.vue
@@ -64,11 +64,12 @@
             <template v-slot:operate>
               <div ref="button" v-if="true" style="margin-bottom:8px">
                 <a-button type="primary" size="small" @click="SelectTranslateType">批量选择语言</a-button>
-                <a-modal title="选择语言" :visible="translateTypeVisible" @ok="confirmTranslateType" @cancel="cancelTranslateType">
+                <a-modal style="width: 320px;" class="choiceLang" centered title="选择语言" :visible="translateTypeVisible" @ok="confirmTranslateType"
+                  @cancel="cancelTranslateType">
                   <div style="text-align: center;">
-                  <a-select v-model:value="selectedLanguage" style="width: 186px;" placeholder="请选择内容" :options='translateTypes'
-                    :fieldNames="{label:'name',value:'name'}">
-                  </a-select>
+                    <a-select v-model:value="selectedLanguage" style="width: 100%;" placeholder="请选择内容" :options='translateTypes'
+                      :fieldNames="{label:'name',value:'name'}">
+                    </a-select>
                   </div>
                 </a-modal>
               </div>
@@ -643,6 +644,13 @@ export default {
   width: 100%;
   height: 100%;
   padding: 16px;
+}
+.choiceLang ::v-deep .ant-modal-footer {
+  text-align: center!important
+  // button {
+  //   display: inline-block;
+  //   margin: 0 5px; /* 可选，添加按钮之间的间距 */
+  // }
 }
 .ant-row {
   height: 100%;
