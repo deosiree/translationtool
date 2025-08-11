d18c5e9647ba11ed8bd74c4bde26e44650682e42
惠岩 <1692592987@qq.com>
2025-08-11 16:03:55 +0800

    fit(术语库)：术语库-详情的翻译状态显示不对

--- translation/src/views/glossary/relationModal.vue
+++ translation/src/views/glossary/relationModal.vue
@@ -21,11 +21,25 @@
                 <a-badge color="#36BF7D" /><span style="color:#36BF7D">已审核</span>
               </template>
             </template>
+            <template v-if="column.dataIndex === 'translateState'">
+              <template v-if="record[column.dataIndex] === '0'">
+                <a-badge color="#6BB8FF" /><span style="color:#6BB8FF">未翻译</span>
+              </template>
+              <template v-if="record[column.dataIndex] === '1'">
+                <a-badge color="#FBB31F" /><span style="color:#FBB31F">待审核</span>
+              </template>
+              <template v-if="record[column.dataIndex] === '2'">
+                <a-badge color="#ff0000" /><span style="color:#ff0000">审核不通过</span>
+              </template>
+              <template v-if="record[column.dataIndex] === '3'">
+                <a-badge color="#36BF7D" /><span style="color:#36BF7D">已审核</span>
+              </template>
+            </template>
             <!--tag列 -->
             <template v-if="column.dataIndex === 'tag'">
               <span>
                 <a-tag color="cyan" class="tag-content">
-                  <span>{{ text }}</span>
+                  <span>{{ record.tag }}</span>
                 </a-tag>
               </span>
             </template>
