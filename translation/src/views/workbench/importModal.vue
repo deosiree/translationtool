964e69d66c24965a525455e17214e7c8278031e1
惠岩 <1692592987@qq.com>
2025-10-21 15:36:09 +0800

    fit(工作台-导入)：增加xml导入方式

--- translation/src/views/workbench/importModal.vue
+++ translation/src/views/workbench/importModal.vue
@@ -604,7 +604,7 @@ export default {
       },
       selectedRowKeys: [],
       selectedRows: [],
-      accept: ".xls,.xlsx,.csv",
+      accept: ".xls,.xlsx,.csv,.xml",
       tsOptions: [],
       selectTitle: "",
       tsFile: {
