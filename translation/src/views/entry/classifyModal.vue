3bb43f5f7d1dd70838dcbadbb34fa6d3d8a1b82e
惠岩 <1692592987@qq.com>
2025-12-03 10:18:16 +0800

    fix(词条管理)：新增产品时，不由前端生成uuidv4并传递key，这可能会导致id重复

--- translation/src/views/entry/classifyModal.vue
+++ translation/src/views/entry/classifyModal.vue
@@ -1,152 +1,140 @@
 <template>
-    <Modal 
-    :modalWidth="modalWidth" 
-    :visible="visible" 
-    :modalTitle="modalTitle"
-    @handleClose="handleClose"
-    @handleOK="handleOK"
-    @afterClose="afterClose"
-    >
-        <div class="content">
-            <a-form
-                ref="formRef"
-                name="custom-validation"
-                autocomplete='off'
-                :model="classify"
-                :label-col="labelCol"
-            >
-                <a-form-item label="名称" name="title"
-                    :rules="[{ required: true, message: '请输入名称!' }]"
-                >
-                    <a-input v-model:value="classify.title" placeholder="请输入内容"></a-input>
-                </a-form-item>
-                <a-form-item v-if="modalTitle === '添加模块' || modalTitle === '编辑模块'" label="词条字符数" name="maxByte">
-                    <a-input-number v-model:value="classify.maxByte" placeholder="请输入词条最大字符数" style="width:100%"></a-input-number>
-                </a-form-item>
-                <a-form-item v-if="modalTitle === '添加模块' || modalTitle === '编辑模块'" label="翻译字符数" name="foreignMaxByte">
-                    <a-input-number v-model:value="classify.foreignMaxByte" placeholder="请输入翻译最大字符数" style="width:100%"></a-input-number>
-                </a-form-item>
-            </a-form>
-        </div>
-    </Modal>
+  <Modal :modalWidth="modalWidth" :visible="visible" :modalTitle="modalTitle" @handleClose="handleClose" @handleOK="handleOK"
+    @afterClose="afterClose">
+    <div class="content">
+      <a-form ref="formRef" name="custom-validation" autocomplete='off' :model="classify" :label-col="labelCol">
+        <a-form-item label="名称" name="title" :rules="[{ required: true, message: '请输入名称!' }]">
+          <a-input v-model:value="classify.title" placeholder="请输入内容"></a-input>
+        </a-form-item>
+        <a-form-item v-if="modalTitle === '添加产品' || modalTitle === '编辑产品'" label="版本" name="title" :rules="[{ message: '请输入版本(对应代码分支)!' }]">
+          <a-input v-model:value="classify.codeBranch" placeholder="请输入版本(对应代码分支)"></a-input>
+        </a-form-item>
+        <a-form-item v-if="modalTitle === '添加模块' || modalTitle === '编辑模块'" label="词条字符数" name="maxByte">
+          <a-input-number v-model:value="classify.maxByte" placeholder="请输入词条最大字符数" style="width:100%"></a-input-number>
+        </a-form-item>
+        <a-form-item v-if="modalTitle === '添加模块' || modalTitle === '编辑模块'" label="翻译字符数" name="foreignMaxByte">
+          <a-input-number v-model:value="classify.foreignMaxByte" placeholder="请输入翻译最大字符数" style="width:100%"></a-input-number>
+        </a-form-item>
+      </a-form>
+    </div>
+  </Modal>
 </template>
 <script>
-import Modal from '@/components/modal/index.vue';
-import { 
-    addEntryClassfy,
-    updateEntryClassfy
-} from "@/http/api/entryManage";
-import { 
-    addProduct,
-    updateProduct
-} from "@/http/api/product";
-import { message } from 'ant-design-vue';
-import { v4 as uuidv4 } from 'uuid';
+import Modal from "@/components/modal/index.vue";
+import { addEntryClassfy, updateEntryClassfy } from "@/http/api/entryManage";
+import { addProduct, updateProduct } from "@/http/api/product";
+import { message } from "ant-design-vue";
+import { v4 as uuidv4 } from "uuid";
 export default {
-    components:{
-        Modal
+  components: {
+    Modal,
+  },
+  emits: ["classifyClose"],
+  props: {
+    visible: {
+      type: Boolean,
+      default: false,
     },
-    emits:['classifyClose'],
-    props: {
-        visible:{
-            type: Boolean,
-            default: false
-        },
-        modalTitle:{
-            type:String
-        },
-        currentClass:{}
+    modalTitle: {
+      type: String,
     },
-    data() {
-        return{
-            labelCol: { style: { width: '80px' } },
-            modalWidth:"400px",
-            classify:{
-                title:"",
-                maxByte:"",
-                foreignMaxByte:""
-            }
-        }
-    },
-    
-    created() {
-        
-    },
-    mounted () {
-        this.classify = this.currentClass
-    },
-    watch: {
-        currentClass(newval,oldval){
-            this.classify = newval
-        }
-    },
-    methods: {
-        handleClose(){
-            this.$emit("classifyClose", true);
-        },
-        handleOK(){
-            this.$refs.formRef.validate().then(() => {
-                if(this.modalTitle === '添加分类' || this.modalTitle === '添加模块'){
-                    addEntryClassfy(this.classify).then((res) => {
-                        message.success('新增成功！')
-                        this.$emit("classifyClose")
-                    })
-                }else if(this.modalTitle === '编辑分类' || this.modalTitle === '编辑模块'){
-                    updateEntryClassfy(this.classify).then((res) => {
-                        message.success("编辑成功！")
-                        this.$emit("classifyClose")
-                    })
-                }else if(this.modalTitle === '添加产品'){
-                    this.classify.key = uuidv4()
-                    // 产品表添加产品
-                    let data = {
-                        id:this.classify.key,
-                        name:this.classify.title,
-                        parentId: this.classify.parentId
-                    }
-                    addProduct(data).then((res) => {
-                        // 分类表添加产品
-                        addEntryClassfy(this.classify).then((res) => {
-                            message.success('添加成功！')
-                            this.$emit("classifyClose")
-                        })
-                    })
+    currentClass: {},
+  },
+  data() {
+    return {
+      labelCol: { style: { width: "80px" } },
+      modalWidth: "400px",
+      classify: {
+        title: "",
+        maxByte: "",
+        foreignMaxByte: "",
+        codeBranch: "",
+      },
+    };
+  },
 
-                }else if(this.modalTitle === '编辑产品'){
-                    updateEntryClassfy(this.classify).then((res) => {
-                        
-                    })
-                    let data = {
-                        id:this.classify.key,
-                        name:this.classify.title,
-                    }
-                    updateProduct(data).then((res) => {
-                        message.success('编辑成功！')
-                        this.$emit("classifyClose")
-                    })
-
-                }
-            }).catch(err => {
-                // console.log('error', err);
+  created() {},
+  mounted() {
+    this.classify = this.currentClass;
+  },
+  watch: {
+    currentClass(newval, oldval) {
+      this.classify = newval;
+    },
+  },
+  methods: {
+    handleClose() {
+      this.$emit("classifyClose", true);
+    },
+    handleOK() {
+      this.$refs.formRef
+        .validate()
+        .then(() => {
+          if (
+            this.modalTitle === "添加分类" ||
+            this.modalTitle === "添加模块"
+          ) {
+            addEntryClassfy(this.classify).then((res) => {
+              message.success("新增成功！");
+              this.$emit("classifyClose");
             });
-            
-        },
-        afterClose(){
-            this.classify.title = ""
-            this.classify.maxByte = ""
-            this.$refs.formRef.clearValidate()
-        }
-    }
-}
+          } else if (
+            this.modalTitle === "编辑分类" ||
+            this.modalTitle === "编辑模块"
+          ) {
+            updateEntryClassfy(this.classify).then((res) => {
+              message.success("编辑成功！");
+              this.$emit("classifyClose");
+            });
+          } else if (this.modalTitle === "添加产品") {
+            // this.classify.key = uuidv4();// 前端加key，可能会重复
+            // 产品表添加产品
+            let data = {
+              id: this.classify.key,
+              name: this.classify.title,
+              parentId: this.classify.parentId,
+              codeBranch: this.classify.codeBranch,
+            };
+            addProduct(data).then((res) => {
+              // 分类表添加产品
+              addEntryClassfy(this.classify).then((res) => {
+                message.success("添加成功！");
+                this.$emit("classifyClose");
+              });
+            });
+          } else if (this.modalTitle === "编辑产品") {
+            updateEntryClassfy(this.classify).then((res) => {});
+            let data = {
+              id: this.classify.key,
+              name: this.classify.title,
+              codeBranch: this.classify.codeBranch,
+            };
+            updateProduct(data).then((res) => {
+              message.success("编辑成功！");
+              this.$emit("classifyClose");
+            });
+          }
+        })
+        .catch((err) => {
+          // console.log('error', err);
+        });
+    },
+    afterClose() {
+      this.classify.title = "";
+      this.classify.maxByte = "";
+      this.$refs.formRef.clearValidate();
+    },
+  },
+};
 </script>
 <style scoped>
-:deep(.ant-form-item-label){
-    width: 85px;
+:deep(.ant-form-item-label) {
+  width: 85px;
 }
-.content{
-    width: 100%;
-    height: 100%;
-    padding: 10px;
-    background-color: #F3F3F3;
+.content {
+  width: 100%;
+  height: 100%;
+  padding: 10px;
+  background-color: #f3f3f3;
 }
 </style>
